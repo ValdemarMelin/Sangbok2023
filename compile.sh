28a712a4ff0ccf7a85346085bb6007ba449d4a76
Oskar <3982323+oskarr@users.noreply.github.com>
2021-09-15 12:26:35 +0200

    Fixed a sidspaltHack bug (hopefully)

--- compile.sh
+++ compile.sh
@@ -2,6 +2,9 @@
 
 export TEXINPUTS=::$(pwd)
 
+# We remove aux files to get a clean compilation.
+rm **/*.aux
+
 # This script compiles all .tex-files in the main directory (that is, not in Äldre/)
 for dir in *\ -\ */
 do
@@ -11,6 +14,8 @@ do
     for file in *.tex
     do
         echo -e " - \e[35mBearbetar fil:\e[0m $dir$file"
+        # First run generates the aux file, then used by the second run.
+        pdflatex -halt-on-error "$file" ||  { echo -e "\e[31mFel vid bearbetning av:\e[0m $dir/$file" ; exit 1; }
         pdflatex -halt-on-error "$file" ||  { echo -e "\e[31mFel vid bearbetning av:\e[0m $dir/$file" ; exit 1; }
     done;
     cd ..;
