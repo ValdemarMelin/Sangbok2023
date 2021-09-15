58cfa275b06bde8d4bc7b3a7c72f3e1872e2d731
Oskar <3982323+oskarr@users.noreply.github.com>
2021-09-15 10:33:24 +0200

    Updated image comparison workflow.

--- .github/scripts/img.sh
+++ .github/scripts/img.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Pre-cleanup
+#echo -e "\e[1;36mRensar upp\e[0m...";
+rm **/*.pdf-*.png
+
 # This script compiles all .pdf-files in the main directory to png (that is, not in Äldre/)
 for dir in *\ -\ */
 do
@@ -16,5 +20,3 @@ done
 #echo -e "\e[1;36mKomprimerar\e[0m till png.txz";
 #echo -e "\e[1;36mKomprimerar\e[0m till png.txz";
 #tar -cJvf png.txz **/*.pdf-*.png
-#echo -e "\e[1;36mRensar upp\e[0m...";
-#rm **/*.pdf-*.png
--- .github/scripts/img_cmp.sh
+++ .github/scripts/img_cmp.sh
@@ -6,15 +6,26 @@ tar -xJ -C cmp/ -f .github/assets/original.png.txz
 
 AE_TOT=0
 FILE_COUNT=0
+NF_FILE_COUNT=0
 
 for file in *\ -\ */*.pdf-*.png
 do
-    echo -e " - \e[35mBearbetar fil:\e[0m $file"
-    compare -metric AE "$file" "cmp/$file" diff.png
-    AE=$(compare -metric AE "$file" "cmp/$file" diff.png || echo 0)
-    echo "AE: "$AE
+    echo -e "\e[35mBearbetar fil:\e[0m $file"
+    if [ ! -f "cmp/$file" ]; then
+        echo -e "  \e[31mFEL:\e[0m Antalet png-filer skiljer sig från originalet."
+        NF_FILE_COUNT=$((NF_FILE_COUNT + 1))
+        continue
+    fi
+
+    AE=$(compare -metric AE "$file" "cmp/$file" "$file.diff.png" &> /dev/stdout)
+    echo "  Absolute error: $AE"
     AE_TOT=$((AE_TOT + AE))
     FILE_COUNT=$((FILE_COUNT + 1))
 done;
-echo "Files checked: "$FILE_COUNT
-echo "Total Absolute Error: "$AE_TOT
+echo "Files checked: $FILE_COUNT"
+echo "Files not found in original: $NF_FILE_COUNT"
+echo "Total Absolute Error: $AE_TOT"
+
+# if [[ "$NF_FILE_COUNT" -gt 1 ]]; then
+#     exit 1
+# fi
